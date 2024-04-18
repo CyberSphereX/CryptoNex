1d605c2fb79a011ea3b37b190df794aabd19b805
Muhammad Rehan Shafqat <rehanshafqat2004@gmail.com>
2024-04-18 17:38:07 +0500

    dependencies

---
+++ CryptoNex/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <div className='text-lg  flex align-middle justify-between justify-around  '>
+        <div className=" bg-red-500 w-24 h-24  "></div>
+        <div className="bg-green-400 w-24 h-24"></div>
+        <div className="bg-blue-400 w-24 h-24"></div>
+        <div className="bg-black w-24 h-24"></div>
+        <div className=" bg-orange-500 w-24 h-24"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
